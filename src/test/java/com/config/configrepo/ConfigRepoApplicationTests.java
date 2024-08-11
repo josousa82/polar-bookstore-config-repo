1f50f3800746ffa56969b67087dc5966ca1e9737
josousa82 <mfgas2@ou.ac.uk>
2024-08-11 22:27:10 +0100

    Setup of config server.

---
+++ src/test/java/com/config/configrepo/ConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.configrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
